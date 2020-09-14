ab7c284b7e40f8c0422a04b529472d255eb78f79
arthur_wxy <kzzn@qq.com>
2020-09-14 17:13:34 +0800

    delete duplicats dir

--- SpaceX/main/test.py
+++
@@ -1,8 +0,0 @@
-import yaml
-import os
-
-
-with open('../config/config.yaml', 'r') as f:
-    a = yaml.load(f.read(), Loader=yaml.FullLoader)
-
-    print(type(a))
--- SpaceX/test.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from main.logger import info
-
-
-def just_4_test(msg):
-    print('{} just begin'.format(msg))
-    time.sleep(3)
-    print('done!')
