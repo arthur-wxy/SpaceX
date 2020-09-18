25597d2a30cdc89424e9c04c8c22a43848335c8d
arthur_wxy <kzzn@qq.com>
2020-09-18 15:31:20 +0800

    加入了app前端发布

--- SpaceX-Back/main/startDeploy.py
+++ SpaceX-Back/main/startDeploy.py
@@ -5,12 +5,13 @@ import yaml
 import time
 import datetime
 import logging
+import unittest
 
 from main.server_conn import SSHConnect
 from main.para_test import zip_dir
 from main.appBackEndRelease import compile_sln
 from main.appBackUpload import copy_dll
-from .logger import info
+from main.logger import info
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
@@ -78,8 +79,8 @@ def connect_and_unzip(server_list, src_path, dst_path, cmd):
             s.exec_cmd(cmd)
             s.disconnect()
         except Exception as e:
-            print(e)
-            return {'stateCode': '202', 'Msg': 'failed'}
+            # print(e)
+            return {'stateCode': '202', 'Msg': 'failed,{}'.format(e)}
     return {'stateCode': '200', 'Msg': 'success'}
 
 
@@ -117,12 +118,11 @@ def portal_back_release(server_list, branch, dll_list):
     connect_and_unzip(server_list, src_path, dst_path, cmd)
 
 
-def portal_front_release(server_list, branch, dll_list):
+def portal_front_release(server_list, branch, file_list):
     """
 
     :param server_list: 要发布的服务器列表
     :param branch: 要发布的服务器
-    :param dll_list:
     :return:
     """
     pass
@@ -135,17 +135,21 @@ def portal_front_release(server_list, branch, dll_list):
         src_path = a['path']['portal_front_src_path']
         dst_path = a['path']['portal_front_dst_path']
         cmd = a['path']['cmd3']
+    try:
+        # 拉取代码
+        code_pull(repo_path, branch)
+        time.sleep(1)
+        # 前端构建
+        front_build(0, portal_vue_path)
+        time.sleep(1)
+        # 直接打包到tmp目录
+        zip_dir('pfront', portal_vue_build_path, tmp_files_path)
+        # 连接服务器，传输zip，解压
+        connect_and_unzip(server_list, src_path, dst_path, cmd)
+        return {'stateCode': '200', 'Msg': 'success'}
+    except Exception as e:
+        return {'stateCode': '202', 'Msg': 'failed,{}'.format(e)}
 
-    # 拉取代码
-    code_pull(repo_path, branch)
-    time.sleep(1)
-    # 前端构建
-    front_build(0, portal_vue_path)
-    time.sleep(1)
-    # 直接打包到tmp目录
-    zip_dir('pfront', portal_vue_build_path, tmp_files_path)
-    # 连接服务器，传输zip，解压
-    connect_and_unzip(server_list, src_path, dst_path, cmd)
 
 def app_back_release(server_list, branch, dll_list):
     """
@@ -165,19 +169,73 @@ def app_back_release(server_list, branch, dll_list):
         src_path = a['path']['app_back_src_path']
         dst_path = a['path']['app_back_dst_path']
         cmd = a['cmd']['cmd2']
+    try:
+        # 拉取代码
+        code_pull(repo_path, branch)
+        time.sleep(1)
+        # 编译解决方案
+        compile_sln(sln_path)
+        time.sleep(1)
+        # 拷贝到临时目录
+        copy_dll(dll_path, app_tmp_path, dll_list)
+        # 打包
+        zip_dir('ABackRelease', app_tmp_path, tmp_files_path)
+        # 连接服务器并传输zip、解压到对应目录
+        connect_and_unzip(server_list, src_path, dst_path, cmd)
+        return {'stateCode': '200', 'Msg': 'success'}
+    except Exception as e:
+        return {'stateCode': '202', 'Msg': 'failed,{}'.format(e)}
 
-    # 拉取代码
-    code_pull(repo_path, branch)
-    time.sleep(1)
-    # 编译解决方案
-    compile_sln(sln_path)
-    time.sleep(1)
-    # 拷贝到临时目录
-    copy_dll(dll_path, app_tmp_path, dll_list)
-    # 打包
-    zip_dir('ABackRelease', app_tmp_path, tmp_files_path)
-    # 连接服务器并传输zip、解压到对应目录
-    connect_and_unzip(server_list, src_path, dst_path, cmd)
+
+def app_front_release(server_list, branch, file_List):
+    """
+    app的前端发布
+    :param server_list:
+    :param branch:
+    :param file_List:
+    :return:
+    """
+    # 读取一下配置文件
+    with open('../config/config.yaml', 'r', encoding='UTF-8') as f:
+        a = yaml.load(f.read(), Loader=yaml.FullLoader)
+        repo_path = a['path']['app_front_path']
+        app_vue_path = a['path']['app_vue_path']
+        app_angular_Path = a['path']['app_angular_path']
+        app_vue_build_path = a['path']['app_vue_build_path']
+        app_angular_build_Path = a['path']['app_angular_build_Path']
+        tmp_files_path = a['path']['tmp_files_path']
+        app_vue_src_path = a['path']['app_vue_src_path']
+        app_vue_dst_path = a['path']['app_vue_dst_path']
+        app_angular_src_path = a['path']['app_angular_src_path']
+        app_angular_dst_path = a['path']['app_angular_dst_path']
+        cmd_4_vue = a['cmd']['cmd4']
+        cmd_4_angular = a['cmd']['cmd5']
+    try:
+        # 拉取代码、前端构建
+        code_pull(repo_path, branch)
+        time.sleep(1)
+        if 'angular' not in file_List:
+            front_build(0, app_vue_path)
+
+        else:
+            front_build(0, app_vue_path)
+            front_build(1, app_angular_Path)
+            time.sleep(1)
+        # 将前端文件打包到tmp下
+        zip_dir('vue', app_vue_build_path, tmp_files_path)
+        zip_dir('angular', app_angular_build_Path, tmp_files_path)
+        # 连接服务器，传输zip，解压
+        for i in server_list:
+            s = SSHConnect(hostname=i)
+            s.connect()
+            s.upload(app_vue_src_path, app_vue_dst_path)
+            s.upload(app_angular_src_path, app_angular_dst_path)
+            s.exec_cmd(cmd_4_vue)
+            s.exec_cmd(cmd_4_angular)
+            s.disconnect()
+        return {'stateCode': '200', 'Msg': 'success'}
+    except Exception as e:
+        return {'stateCode': '202', 'Msg': 'failed,{}'.format(e)}
 
 
 def download_and_unzip(src_path, target_path):
@@ -185,13 +243,21 @@ def download_and_unzip(src_path, target_path):
 
 
 # 测试
+class TestCase(unittest.TestCase):
+
+    def test_app_front(self):
+        self.assertEqual(app_front_release(0, 0, ['vue']), 1)
+        self.assertEqual(app_front_release(0, 0, ['vue', 'angular']), 2)
+
+
 if __name__ == '__main__':
-    server_list = ['10.151.66.60']
-    branch = '200929feature-TEST'
-    dll_list = ['RekTec.XStudio.Crm.WebApi.dll',
-                'RekTec.XStudio.Exam.WebApi.dll'
-                ]
-    # portal 后端发布
-    portal_back_release(server_list, branch, dll_list)
-    # app 后端发布
-    app_back_release(server_list, branch, dll_list)
+    unittest.main()
+    # server_list = ['10.151.66.60']
+    # branch = '200929feature-TEST'
+    # dll_list = ['RekTec.XStudio.Crm.WebApi.dll',
+    #             'RekTec.XStudio.Exam.WebApi.dll'
+    #             ]
+    # # portal 后端发布
+    # portal_back_release(server_list, branch, dll_list)
+    # # app 后端发布
+    # app_back_release(server_list, branch, dll_list)
